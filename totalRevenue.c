1cf1ed08a972743e6f169e17b8f0408126df5be9
Tajidin Abd <tajidinabd@archlinux.us>
2013-02-11 14:41:24 -0700

    changes for csc119 assignment

--- totalRevenue.c
+++ totalRevenue.c
@@ -6,6 +6,7 @@ Create a program that will multiply price and quantity to calculate
 the total revenue when user input amount by declaring variables and using "scanf" function
 */
 #include <stdio.h> //Include the standard inputoutput header file
+#include <math.h> //Include the math library for rounding the numbers
 
 int main (void) //Declare the function will return integer value
 {
@@ -17,12 +18,12 @@ int main (void) //Declare the function will return integer value
 //	Statements
 	printf("This program will multiply\n"); // Displays what program will do 
 	printf("Enter two numbers\n"); // How many numbers must be entered
-	printf("In the form: nn nn <return>\n"); // Gives the format to input
+	printf("In the form: nn nnnn <return>\n"); // Gives the format to input
 	scanf("%d %f", &t_otal, &p_rice); //Intiallizes the two variables from above
 	
-	sum = t_otal * p_rice; // Number that will be multiplied together t_otal, P_rice
+	sum = t_otal * p_rice; // Number that will be multiplied together t_otal, p_rice
 	
-	printf("The total is: %f\n\n", sum); //Displays the calculated amonunt on the screen
+	printf("The total is: %.2f\n\n", sum); //Displays the calculated amonunt on the screen
 	
 	printf("Thank you.\n"); // Gives an ending greeting to user
 	return 0; //returns a value to the OS upon completion
