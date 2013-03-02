83804f66d031fb82a5378dbf5fdbe8deb1455621
Tajidin Abd <tajidinabd at arch-linux dot us>
2013-03-01 22:08:58 -0700

    Changes to Assignment

--- numbergame.c
+++ numbergame.c
@@ -1,30 +1,39 @@
 /* Topic 5 Assignment 3 Number Game
 Derrick Williams
 CSC119003
-This game will store a random number each time it starts between 1 and 10 and give the user 3 chances
-to pick the correct number otherwise the game will end and tell user to try again. 
+This game will store a random number each time it starts between 1 and 10 and give the user 1 chance
+to pick the correct number otherwise the game will end and display the correct number
 */
 #include <stdio.h>
 #include <ctype.h>
 #include <stdlib.h>
 #include <time.h>
 
-
+// Global Declarations
 int main(void)
 {
+	// Local Declarations
 	int nrange;
-	int R_Eply = 0;
-	char cAnswer = '\0';
+	char R_eply;
 	
+	// Statements
 	srand(time(NULL));
 	nrange = (rand() % 10) + 1;
 
 	printf("\nChoose a number between 1 and 10: ");
-	scanf("%d", &R_Eply);
-{
-	if (R_Eply == nrange)
-		printf("\nYou responded correctly\n");
-	printf("%d",	rand() % nrange + 1);
+	scanf("%c", &R_eply);
+		if (!isdigit(R_eply))  {
+			printf("\nYou must input a digit\n");
+		return 0;
+		}
+		if ( (R_eply - '0') == nrange) {
+			printf("\nYou responded correctly\n");
+			}
+		else {
+		printf("\nSorry you guessed wrong\n");
+		printf("\nThe correct guess was %d\n", nrange);
+   
 	
-	return 0;
+	}
+return 0;
 } //Main
