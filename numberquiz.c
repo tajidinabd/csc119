77a6f3b538f45e93d46b78ce64b94dbd38c86ddd
Tajidin Abd <tajidinabd at arch-linux dot us>
2013-03-13 12:54:48 -0600

    changes to counter and add numberquiz

---
+++ numberquiz.c
@@ -0,0 +1,7 @@
+/* Assignment # 6 Arthmetic Quiz
+Derrick Williams (Abu Bakr)
+CSC119003
+Jumada Al-Awwal 1, 1434 AH  (March 13, 2013 CE
+This program will test the users knowledge of 5 arthmetic questions. 
+Will keep track of number of correct and incorrect questions. 
+ */
