794d4498e13fdce7a4b4a96d17edf72ccd856b34
Tajidin Abd <tajidinabd at arch-linux dot us>
2013-03-13 13:52:31 -0600

    changes to numberquiz

--- numberquiz.c
+++ numberquiz.c
@@ -6,3 +6,11 @@ This program will test the users knowledge of 5 arthmetic questions.
 Will tell user whether answer is correct or incorrect
 Will keep track of number of correct and incorrect questions. 
  */
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main()
+{
+	int wrong_Question, correct_Question, asked_Question;
+	int random_Question, asked_Questions, total_Question;
+}
