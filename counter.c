41159c031f053a4c56897ac555b948267bf263ac
Tajidin Abd <tajidinabd at arch-linux dot us>
2013-03-13 12:43:10 -0600

    changes to counter

--- counter.c
+++ counter.c
@@ -1,8 +1,15 @@
+/* Assignment # 5 Number Counter
+ * Derrick Williams (Abu Bakr)
+ * CSC119003
+ * Jumada Al-Awwal 1, 1434 AH  (March 13, 2013 CE)
+ * This program will count from 1 to 101 in increments of 5 using for loop
+ */ 
 #include <stdio.h>
 int main (void)
 {
+// Statements
 	for(int i = 1; i <= 101; i+=5) {
 		printf ("\t%d\n", i);
 	}
 	return 0;
-}
+} //main
