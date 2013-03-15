0452490f683e5e9352978688cfeab944a638a3f1
Tajidin Abd <tajidinabd at arch-linux dot us>
2013-03-15 13:12:51 -0600

    numberquiz changes

--- numberquiz.c
+++ numberquiz.c
@@ -12,8 +12,28 @@ Will keep track of number of correct and incorrect questions.
 #include <time.h>
 int main()
 {
-	int wrong_Question, correct_Question, asked_Question;
-	int random_Question, asked_Questions, total_Question;
+// Local Declarations
+	int wrong_Question; 
+	int correct_Question; 
+	int asked_Question;
+	int answer_Question;
+	int random_Question;  
+	int total_Question;
+	
+// Statements	
 	printf("\t\t===NumberQuiz===");
 	printf("\n\nEnter a number between 1-5 for the number of Questions");
+	scanf("%d", &total_Question);
+	printf("\nYou will have %d questions for this quiz.", total_Question);
+	
+	srand(time(NULL)); // will use system time to generate truly random
+	
+	random_Question = (rand() %5) +1;  // will generate random question from the 5 choices below in switch statement
+	
+	// Intialize all the variables to zero that will be used in the switch statement
+	wrong_Question = 0
+	correct_Question = 0
+	asked_Question = 0
+	answer_Question = 0
+	
 }
