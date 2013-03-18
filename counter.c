e8bbab243c12f8bd8a2dc923a729f0a82cc1b96b
Tajidin Abd <tajidinabd@archlinux.us>
2013-03-18 09:33:57 -0600

    Update counter.c
    add more comments

--- counter.c
+++ counter.c
@@ -8,8 +8,8 @@ This program will count from 1 to 101 in increments of 5 using for loop
 int main (void)
 {
 // Statements
-	for(int i = 1; i <= 101; i+=5) {
-		printf ("\t%d\n", i);
+	for(int i = 1; i <= 101; i+=5) { // initializes variable to 1 set condition and increments by 5
+		printf ("\t%d\n", i); // displays varaiable tabs over on a new line
 	}
 	return 0;
 } //main
