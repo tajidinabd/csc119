26fd6928eded55d299f31187cd946cde6feb8bf8
Tajidin Abd <tajidinabd at arch-linux dot us>
2013-03-25 15:15:01 -0600

    more code building on quizgame.c

--- quizgame.c
+++ quizgame.c
@@ -7,22 +7,44 @@ also will notify user whether answer is correct or incorrect
 also at the end of the quiz give user total score for the Quiz.
 */
 #include <stdio.h>
-
+#include <ctype.h>
+//User Defined Function
 void playquiz(void)
 {
-	printf("\tPlay Quiz");
+	printf("\tTake the Quiz");
 }
 
 int main(void)
 {
+//Local Declarations
 int score[0] = {0}; 
 int asked[0] = {0};	
 int input;
 int answer;
-
-	printf( "\t1. Play game\n" );
+	
+	//Statements
+	printf( "\t1. Play game!!\n" );
 	printf( "\t2. Exit\n" );
 	printf( "\tSelection: \n" );
     scanf( "%d", &input );
-
-}
+	
+	if (isdigit(input)) {
+		printf("\nYou must input a digit\n");
+	} //if
+	
+	//Switch 
+	switch (input) {
+		case 1:            
+		playgame();
+		break;
+		case 2:        
+            printf( "Thanks for playing!\n" );
+            break;
+        default:            
+            printf( "Bad input, quitting!\n" );
+            break;
+	} //switch
+	getchar();
+	
+	
+} //main
