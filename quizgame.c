0fa6090a6e16e1846a133a1adca4ad07b68abc3c
Tajidin Abd <tajidinabd at arch-linux dot us>
2013-03-25 09:39:57 -0600

    beginning quiz game code

--- quizgame.c
+++ quizgame.c
@@ -6,3 +6,23 @@ This Quiz Game will give user choice of three categories to choose from
 also will notify user whether answer is correct or incorrect 
 also at the end of the quiz give user total score for the Quiz.
 */
+#include <stdio.h>
+
+void playquiz(void)
+{
+	printf("\tPlay Quiz");
+}
+
+int main(void)
+{
+int score[0] = {0}; 
+int asked[0] = {0};	
+int input;
+int answer;
+
+	printf( "\t1. Play game\n" );
+	printf( "\t2. Exit\n" );
+	printf( "\tSelection: \n" );
+    scanf( "%d", &input );
+
+}
